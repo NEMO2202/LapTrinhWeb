366368f92bae6d7ad3a6e4eb4c12e017d1e869dd
phamly <phamthily2222002@gmail.com>
2023-06-25 10:10:40 +0700

    commit of nemo

---
+++ LayoutMinishopMaster/LayoutMinishopMaster/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LayoutMinishopMaster")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LayoutMinishopMaster")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("53d7a429-97dd-4f50-bf3a-be1d5f2a912b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LayoutMinishopMaster/LayoutMinishopMaster/ViewModel/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LayoutMinishopMaster.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LayoutMinishopMaster.ViewModel
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<PhanLoai> PhanLoais { get; set; }
+        public IEnumerable<SanPham> SanPhams { get; set; }
+    }
+}
